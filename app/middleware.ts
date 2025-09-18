cea14ec0954d9325bd631da0313d0890350f011d
davidadityawinarto <davidadityawinarto@gmail.com>
2025-09-18 14:14:55 +0700

    test build

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,10 +1,9 @@
 //file ini harus berada di app
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
-import Cookies from "js-cookie";
 
 export function middleware(request: NextRequest) {
-    const token = Cookies.get("logged_in");
+    const token = request.cookies.get("logged_in")?.value;
 
     //jika tidak ada token dan mengakses halaman selain login maka redirect ke halaman login
     if (!token && request.nextUrl.pathname !== "/login") {
@@ -12,9 +11,9 @@ export function middleware(request: NextRequest) {
     }
 
     return NextResponse.next();
-
-    //buat daftar alamat yang akan diprotect
-    export const config = {
-        matcher: ["/crud"],
-    };
 }
+
+//buat daftar alamat yang akan diprotect
+export const config = {
+    matcher: ["/crud"],
+};
