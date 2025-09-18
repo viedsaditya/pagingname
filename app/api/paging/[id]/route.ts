b102e5c4622f9171bab6870adc28b18f7de9bae5
davidadityawinarto <davidadityawinarto@gmail.com>
2025-09-18 14:04:35 +0700

    test build

---
+++ app/api/paging/[id]/route.ts
@@ -0,0 +1,31 @@
+//impelementasi CRUD API NEXTJS PRISMA & MYSQL
+import { NextResponse, type NextRequest } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+//API untuk menampilkan data paging by id
+export async function GET(
+    request: NextRequest,
+    context: { params: { id: string } }
+) {
+    const { params } = context;
+
+    if (!params || !params.id) {
+        return NextResponse.json({ message: "ID harus diisi" }, { status: 400 });
+    }
+
+    const paging = await prisma.tb_paging.findUnique({
+        where: {
+            id: parseInt(params.id)
+        }
+    });
+
+    if (!paging) {
+        return NextResponse.json({ message: "ID Tidak Ditemukan" }, { status: 404 });
+    }
+
+    return NextResponse.json(paging, { status: 200 });
+}
+
+
